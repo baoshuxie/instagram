5a99472aab71d8481e0f44a2181c17c98880ca97
baoshuxie <swufe_ljj@163.com>
2018-07-16 13:32:29 +0800

    0716

--- instagram/instagram/spiders/instaphoto.py
+++ instagram/instagram/spiders/instaphoto.py
@@ -12,63 +12,40 @@ import math
 class InstaphotoSpider(scrapy.Spider):
 	name = 'instaphoto'
 	allowed_domains = ['instagram.com']
-	to_Parselist = []
+	toParseList = []
+	ParsedSet = set()
+	id = 1
 	#start_urls = ['http://instagram.com/']
 
 	#使用 PhontomJS 完成登录
-	def __init__(self):
-		self.browser = webdriver.PhantomJS()
-		print('OJBK')
-		self.browser.get('https://www.instagram.com/accounts/login/')
-		self.browser.find_element_by_name('username').clear()
-		self.browser.find_element_by_name('password').clear()
-		self.browser.find_element_by_name('username').send_keys('luojunjie20')
-		self.browser.find_element_by_name('password').send_keys('19950708')
-		time.sleep(1)
-		
+	def start_requests(self,response):
+		with open('/Users/junjieluo/MyGit/instagram/toParse.txt','w+') as f:
+			toParse = f.read()
 
-		with open('/Users/junieluo/MyGit/Instagram/toParse.txt','w+') as f:
-			to_Parse = f.read()
+		self.toParseList = toParse.split('  ')[:-1]
 
-		self.to_Parselist = to_Parse.split('  ')[:-1]
+		with open('/Users/junjieluo/MyGit/instagram/Parsed.txt','w+')
+			Parsed = f.read()
+	
+		self.ParsedSet = set(Parsed.split('  ')[:-1])
 
-	def closed(self,spider):
-		self.browser.close()
-		print('OJBK1')
-		
+		for username in self.toParseList:
+			if username in self.ParsedSet:
+				continue
+			else:
+				self.toParseList.remove(username)
+				self.ParsedSet.add(username)
+				self.id += 1
+				url = 'http://www.instagram.com/%s/'%username
+				yield Request(url,
+					meta = {'username':username}
+					callback = self.parse_photo_urls)
 
-	def start_requests(self):
-		for element in self.to_Parselist:
-			yield [Request('www.instagram.com/%s/'%element,
-				callback = self.get_user)]
-			#return [Request('http://www.baidu.com/',
-			 
-	'''def login(self,response):
-		print('OJBK1')
-		return [FormRequest.from_response(response,formdata={
-			'username':'luojunjie20',
-			'password':'19950708'
-			},
-			callback = self.get_user,
-			dont_filter = True
-			)]'''
+	def parse_photo_urls(self,response):
+		photo_urls = response.xpath()
 
-
-	#获取待处理用户
-	def get_user(self,response):
-		print('OJBK2')
-		print(response.text)
-
-		
-
-	#获取关注者
-	#def get_followers(self,response):
-
-	#获取关注者的主页
-	#def get_pages(self,response):
-
-	#获取相片 url
-	#def get_urls(self,response):
+		for photo_url in photo_urls:
+			item = 
 
 
 
